3d7e420f0a4119760b48da605535df9fec457428
jhirschv <johnhirschv@gmail.com>
2024-02-16 15:15:10 -0500

    added create program fucntion

--- src/_root/pages/Programs.jsx
+++ src/_root/pages/Programs.jsx
@@ -8,24 +8,60 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+AlertDialog,
+AlertDialogAction,
+AlertDialogCancel,
+AlertDialogContent,
+AlertDialogDescription,
+AlertDialogFooter,
+AlertDialogHeader,
+AlertDialogTitle,
+AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+  
 import { Button } from "@/components/ui/button"
 import { useTheme } from '@/components/theme-provider';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function YourWorkouts() {
-
     const { theme } = useTheme();
-  
     const backgroundColorClass = theme === 'dark' ? 'bg-popover' : 'bg-secondary';
 
+    const [programName, setProgramName] = useState("");
+    const [programDescription, setProgramDescription] = useState("");
+
+    const handleNameInputChange = (event) => {
+    setProgramName(event.target.value); // Update state with input value
+        };
+    const handleDescriptionInputChange = (event) => {
+        setProgramDescription(event.target.value); // Update state with input value
+        };
+    const resetForm = () => {
+        setProgramName('');
+        setProgramDescription('');
+        };
+
+    function createProgram() {
+        const programData = {
+            name: programName, // Assuming your API expects a key called 'name'
+            description: programDescription, // Assuming your API expects a key called 'description'
+        };
+    
+        apiClient.post('/programs/', programData) // Pass programData as the payload in the POST request
+        .then(response => {
+            setPrograms(currentPrograms => [...currentPrograms, response.data]); // Update your state or context with the response
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    }
+
     const [programs, setPrograms] = useState([]);
-    const navigate = useNavigate();
-
-    const handleProgramClick = (programId) => {
-        navigate(`/programs/${programId}`); // Navigate to program details page
-    };
-
 
     useEffect(() => {
         apiClient.get('/user_programs/') // Make sure the endpoint matches your Django URL configuration
@@ -36,6 +72,11 @@ export default function YourWorkouts() {
             console.error('Error fetching data:', error);
         });
     }, []);
+
+    const navigate = useNavigate();
+    const handleProgramClick = (programId) => {
+        navigate(`/programs/${programId}`); // Navigate to program details page
+    };
 
     return (
 
@@ -62,7 +103,22 @@ export default function YourWorkouts() {
                     
                     </div>
                 </div>
-                <Button onClick={()=>navigate('/create')} className='m-4 bg-muted-foreground'>Create New Program</Button>
+                <AlertDialog>
+                    <AlertDialogTrigger className='ml-4 h-10 px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'>
+                        Create Program
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                        <AlertDialogTitle>Create Program</AlertDialogTitle>
+                        <Label htmlFor="programName">Name</Label><Input onChange={handleNameInputChange} value={programName} autoComplete="off" id="programName" />
+                        <Label htmlFor="description">Description</Label><Input onChange={handleDescriptionInputChange} value={programDescription} autoComplete="off" id="description" />
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                        <AlertDialogCancel onClick={resetForm}>Cancel</AlertDialogCancel>
+                        <AlertDialogAction onClick={createProgram}>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </Card>
         </div>
         
@@ -70,28 +126,3 @@ export default function YourWorkouts() {
     )
  
 }
-
-{/* {programs.map(program => (
-                <div key={program.id}>
-                <h1>{program.name}</h1>
-                <p>{program.description}</p>
-                {program.phases.map(phase => (
-                    <div key={phase.id}>
-                    <h3>Phase {phase.number}: {phase.weeks} weeks</h3>
-                    {phase.workouts.map(workout => (
-                        <div key={workout.id}>
-                        <h4>Workout: {workout.name}</h4>
-                        {workout.workout_exercises.map(exercise => (
-                            <div key={exercise.id}>
-                            <p>Exercise: {exercise.exercise.name}</p>
-                            <p>Sets: {exercise.sets}, Reps: {exercise.reps}</p>
-                            {exercise.note && <p>Note: {exercise.note}</p>}
-                            {exercise.video && <p><a href={exercise.video}>Video</a></p>}
-                            </div>
-                        ))}
-                        </div>
-                    ))}
-                    </div>
-                ))}
-                </div>
-            ))} */}
