0151282c2db0742ab28d0780b3d5e5c4d6434e6c
jhirschv <johnhirschv@gmail.com>
2024-02-21 16:13:35 -0500

    made changes to the program over view page for the better(UI)

--- src/_root/pages/ProgramOverview.jsx
+++ src/_root/pages/ProgramOverview.jsx
@@ -22,11 +22,13 @@ import {
     TableRow,
     } from "@/components/ui/table"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faCirclePlus, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faCirclePlus, faPlus, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
 import { faShareFromSquare } from '@fortawesome/free-regular-svg-icons';
 import { useTheme } from '@/components/theme-provider';
 import { useNavigate } from 'react-router-dom';
 import { Badge } from '@/components/ui/badge'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 
 const ProgramOverview = () => {
     const { theme } = useTheme();
@@ -46,14 +48,212 @@ const ProgramOverview = () => {
                 </div>
                 <Button variant='outline'>Assign Clients</Button>
             </div>
-            <div className='flex w-full px-8 h-20'>
-                <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Accumulation</p><p className='text-sm'>4 Weeks</p></div>
-                <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Intensificaition</p><p className='text-sm'>4 Weeks</p></div>
-                <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Accumulation</p><p className='text-sm'>4 Weeks</p></div>
-                <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Intensificaition</p><p className='text-sm'>4 Weeks</p></div>
-                <div className='flex flex-col justify-center items-center w-24 text-xs'><FontAwesomeIcon className='mb-1 text-muted-foreground' icon={faCirclePlus} size="2xl" /><p className='text-muted-foreground'>Add Phase</p></div>
+            <div className='flex flex-col'>
+                <div className='flex w-full px-6 h-20'>
+                    <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Accumulation 1</p><p className='text-sm'>4 Weeks</p></div>
+                    <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Intensificaition 1</p><p className='text-sm'>4 Weeks</p></div>
+                    <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Accumulation 2</p><p className='text-sm'>4 Weeks</p></div>
+                    <div className='flex flex-col justify-center items-center flex-1 border bg-primary hover:bg-primary-darker'><p className='text-xl font-semibold'>Intensificaition 2</p><p className='text-sm'>4 Weeks</p></div>
+                    <div className='flex flex-col justify-center items-center w-24 text-xs'><FontAwesomeIcon className='mb-1 text-muted-foreground' icon={faCirclePlus} size="2xl" /><p className='text-muted-foreground'>Add Phase</p></div>
+                </div>
+                
             </div>
-            <div className='grid grid-cols-2 gap-4 px-4'>
+            <div className='w-full px-6 pt-4'>
+                <Card className='px-2'>
+                    <div className='flex justify-between items-center'>
+                        <h1 className='p-4 text-xl font-semibold'>Accumulation 1</h1>
+                        <Button variant='outline'>
+                            <FontAwesomeIcon className='mr-1' icon={faPlus} /> Add Workout
+                        </Button>
+                       
+                    </div>
+                    
+                    <Tabs defaultValue="1" className="w-full rounded-sm">
+                        <div className='flex items-center'>
+                            <TabsList className="grid w-full grid-cols-4">
+                                <TabsTrigger value="1">Lower Body 1</TabsTrigger>
+                                <TabsTrigger value="2">Upper Body 1</TabsTrigger>
+                                <TabsTrigger value="3">Lower Body 2</TabsTrigger>
+                                <TabsTrigger value="4">Upper Body 2</TabsTrigger>
+                            </TabsList>
+                        </div>
+                        <TabsContent value="1">
+                            <Card  className='border-none h-80'>
+                            <CardContent className="space-y-2">
+                            <Table className='h-full'>
+                                <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Exercise</TableHead>
+                                    <TableHead>Sets x Reps</TableHead>
+                                    <TableHead>Note</TableHead>
+                                </TableRow>
+                                </TableHeader>
+                                <TableBody onClick={handleProgramClick}>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Back Squat</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                        <TableCell>Build up to a heavy top set</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-48">Romanian Deadlift</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Split Squat</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                        <TableCell>Focus on Form</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Calf Raise</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                            </CardContent>
+                            <CardFooter>
+                                
+                            </CardFooter>
+                            </Card>
+                        </TabsContent>
+                        <TabsContent value="2">
+                            <Card className='border-none h-80'>
+                            <CardContent className="space-y-2">
+                            <Table className='h-full'>
+                                <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Exercise</TableHead>
+                                    <TableHead>Sets x Reps</TableHead>
+                                    <TableHead>Note</TableHead>
+                                </TableRow>
+                                </TableHeader>
+                                <TableBody onClick={handleProgramClick}>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Back Squat</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                        <TableCell>Build up to a heavy top set</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-48">Romanian Deadlift</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Split Squat</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                        <TableCell>Focus on Form</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Calf Raise</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                            </CardContent>
+                            <CardFooter>
+                                
+                            </CardFooter>
+                            </Card>
+                        </TabsContent>
+                        <TabsContent value="3">
+                            <Card className='border-none h-80'>
+                            <CardContent className="space-y-2">
+                            <Table className='h-full'>
+                            <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Exercise</TableHead>
+                                <TableHead>Sets x Reps</TableHead>
+                                <TableHead>Note</TableHead>
+                            </TableRow>
+                            </TableHeader>
+                            <TableBody onClick={handleProgramClick}>
+                                <TableRow>
+                                    <TableCell className="font-medium w-36">Back Squat</TableCell>
+                                    <TableCell>5 x 5</TableCell>
+                                    <TableCell>Build up to a heavy top set</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="font-medium w-48">Romanian Deadlift</TableCell>
+                                    <TableCell>5 x 5</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="font-medium w-36">Split Squat</TableCell>
+                                    <TableCell>3 x 8</TableCell>
+                                    <TableCell>Focus on Form</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className="font-medium w-36">Calf Raise</TableCell>
+                                    <TableCell>3 x 8</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                            </CardContent>
+                            <CardFooter>
+                                
+                            </CardFooter>
+                            </Card>
+                        </TabsContent>
+                        <TabsContent value="4">
+                            <Card className='border-none h-80'>
+                            <CardContent className="space-y-2">
+                            <Table className='h-full'>
+                                <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Exercise</TableHead>
+                                    <TableHead>Sets x Reps</TableHead>
+                                    <TableHead>Note</TableHead>
+                                </TableRow>
+                                </TableHeader>
+                                <TableBody onClick={handleProgramClick}>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Back Squat</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                        <TableCell>Build up to a heavy top set</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-48">Romanian Deadlift</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Split Squat</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                        <TableCell>Focus on Form</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36">Calf Raise</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                            </CardContent>
+                            <CardFooter>
+                                
+                            </CardFooter>
+                            </Card>
+                        </TabsContent>
+                    </Tabs>
+                </Card>
+            
+            </div>
+            
+        </Card>
+    </div>
+  )
+}
+
+export default ProgramOverview
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className='grid grid-cols-2 gap-4 px-4'>
                 <div className='flex my-4 flex-col w-full h-96 flex-grow border rounded-lg'>
                 <h1 className='pt-4 pb-1 text-2xl font-semibold text-center'>Phase Details</h1>
                 <h1 className='text-lg font-semibold text-center text-muted-foreground'>Accumulation</h1>
@@ -122,10 +322,4 @@ const ProgramOverview = () => {
                     </TableBody>
                 </Table>
                 </div>
-            </div>
-        </Card>
-    </div>
-  )
-}
-
-export default ProgramOverview
+            </div> */}
