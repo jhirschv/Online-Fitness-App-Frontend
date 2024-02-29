addaf2eb167032d4f8702e3aeecf75b8d16485ac
jhirschv <johnhirschv@gmail.com>
2024-02-29 13:02:16 -0500

    fixed ui for trianing page

--- src/_root/pages/Train.jsx
+++ src/_root/pages/Train.jsx
@@ -9,6 +9,11 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+  import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+  } from "@/components/ui/popover"
   import { ScrollArea } from "@/components/ui/scroll-area"
   import { Calendar } from "@/components/ui/calendar"
   import { Button } from "@/components/ui/button"
@@ -41,6 +46,8 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
 const Train = () => {
     const { theme } = useTheme();
@@ -56,91 +63,109 @@ const Train = () => {
     const backgroundColorClass = theme === 'dark' ? 'bg-popover' : 'bg-secondary';
 
     return (
-        <div className={`${backgroundColorClass} w-full px-4 pt-4 border rounded-lg`}>
-            <Card className='flex h-auto'>
-                <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                className="flex-1 h-[95%] m-4"
-                />
-                <div className='flex flex-col flex-1 border-l pl-4'>
-                    <div className='flex items-center justify-between pr-2 py-4'>
-                        <h1 className='text-center text-xl font-semibold'>12 Week Undulating</h1>
-                        <Sheet>
-                            <SheetTrigger asChild>
-                                <Button variant="outline">Change Program</Button>
-                            </SheetTrigger>
-                            <SheetContent>
-                                <SheetHeader>
-                                <SheetTitle>Select Program</SheetTitle>
-                                </SheetHeader>
-                                <div className='p-4'>
-                                    <h1>12 Week Linear</h1>
+        <div className={`${backgroundColorClass} w-full p-4 border rounded-lg`}>
+            <Card className='h-full w-full flex flex-col'>
+                <div className='flex h-full'>
+
+                    <div className='flex flex-col h-full justify-between basis-2/5 pl-6'>
+                        <div className='flex flex-col pr-2 py-6'>
+                            <div className='flex mb-4'>
+                                <h1 className='mr-2 text-2xl font-semibold'>12 Week Undulating</h1>
+                                <Sheet>
+                                    <SheetTrigger asChild>
+                                        <Button variant="outline">Change Program</Button>
+                                    </SheetTrigger>
+                                    <SheetContent>
+                                        <SheetHeader>
+                                        <SheetTitle>Select Program</SheetTitle>
+                                        </SheetHeader>
+                                        <div className='p-4'>
+                                            <h1>12 Week Linear</h1>
+                                        </div>
+                                        <Separator></Separator>
+                                        <div className='p-4'>
+                                            <h1>8 Week Hypertrophy</h1>                                  
+                                        </div>
+                                        <Separator></Separator>
+                                        <div className='p-4'>
+                                            <h1>16 Week Linear</h1>                                   
+                                        </div>
+                                        <Separator></Separator>
+                                        <div className='p-4'>
+                                            <h1>McKays Program</h1>                                  
+                                        </div>
+                                        <Separator></Separator>
+                                        <SheetFooter className='mt-4'>
+                                        <SheetClose asChild>
+                                            <Button type="submit">Save changes</Button>
+                                        </SheetClose>
+                                        </SheetFooter>
+                                    </SheetContent>
+                                </Sheet>
+                            </div>
+                            
+                            
+                            <div className='flex items-center justify-between pr-2'>
+                                <h1 className='font-semibold text-lg'>Lower Body 1</h1>
+                                <div className=''>
+                                    <Popover>
+                                        <PopoverTrigger><FontAwesomeIcon icon={faEllipsis} /></PopoverTrigger>
+                                        <PopoverContent>Place content for the popover here.</PopoverContent>
+                                    </Popover>
                                 </div>
-                                <Separator></Separator>
-                                <div className='p-4'>
-                                    <h1>8 Week Hypertrophy</h1>                                  
-                                </div>
-                                <Separator></Separator>
-                                <div className='p-4'>
-                                    <h1>16 Week Linear</h1>                                   
-                                </div>
-                                <Separator></Separator>
-                                <div className='p-4'>
-                                    <h1>McKays Program</h1>                                  
-                                </div>
-                                <Separator></Separator>
-                                <SheetFooter className='mt-4'>
-                                <SheetClose asChild>
-                                    <Button type="submit">Save changes</Button>
-                                </SheetClose>
-                                </SheetFooter>
-                            </SheetContent>
-                        </Sheet>
+                            </div>
+                        
+                            <Table className='h-full'>
+                                <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px] pl-0">Exercise</TableHead>
+                                    <TableHead>Sets x Reps</TableHead>
+                                    <TableHead>Note</TableHead>
+                                </TableRow>
+                                </TableHeader>
+                                <TableBody >
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36 pl-0">Back Squat</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                        <TableCell>Build up to a heavy top set</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-48 pl-0">Romanian Deadlift</TableCell>
+                                        <TableCell>5 x 5</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36 pl-0">Split Squat</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                        <TableCell>Focus on Form</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell className="font-medium w-36 pl-0">Calf Raise</TableCell>
+                                        <TableCell>3 x 8</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </div>
+                        
+                        <div className='mb-6'>
+                            <Button onClick={startTrainingSession} className='self-center w-1/2 p-6 text-lg'>Start Training!</Button>
+                            <Button variant='outline' className='mx-2 self-center w-1/3 mb-4 p-6 text-lg'>Skip Workout</Button>
+                        </div>
+                        
                     </div>
-                    <div className='flex items-center justify-between pr-2'>
-                        <h1 className='font-semibold text-lg'>Week 3: Lower 1</h1>
-                        <Button variant='outline'>All Sessions</Button>
+
+                    <div className='flex h-full items-center justify-center basis-3/5'>
+                        <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        className="h-[90%] m-4"
+                        />
                     </div>
                     
-                    <Table className='h-full'>
-                    <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">Exercise</TableHead>
-                        <TableHead>Sets x Reps</TableHead>
-                        <TableHead>Note</TableHead>
-                    </TableRow>
-                    </TableHeader>
-                    <TableBody >
-                        <TableRow>
-                            <TableCell className="font-medium w-36">Back Squat</TableCell>
-                            <TableCell>5 x 5</TableCell>
-                            <TableCell>Build up to a heavy top set</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className="font-medium w-48">Romanian Deadlift</TableCell>
-                            <TableCell>5 x 5</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className="font-medium w-36">Split Squat</TableCell>
-                            <TableCell>3 x 8</TableCell>
-                            <TableCell>Focus on Form</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell className="font-medium w-36">Calf Raise</TableCell>
-                            <TableCell>3 x 8</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-                 
-                <div className='mt-4'>
-                    <Button onClick={startTrainingSession} className='self-center w-1/2 p-6 text-lg'>Start Training!</Button>
-                    <Button variant='outline' className='mx-2 self-center w-1/3 mb-4 p-6 text-lg'>Skip Workout</Button>
+
+                    
+                    
                 </div>
-                
-                </div>
-                
             </Card>
         </div>
     )
--- src/_root/pages/WorkoutSession.jsx
+++ src/_root/pages/WorkoutSession.jsx
@@ -24,86 +24,112 @@ import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock} from '@fortawesome/free-regular-svg-icons';
+import { faAngleLeft} from '@fortawesome/free-solid-svg-icons';
+import { useTheme } from '@/components/theme-provider';
   
 
 const WorkoutSession = () => {
+
+
+    const { theme } = useTheme();
+    const backgroundColorClass = theme === 'dark' ? 'bg-popover' : 'bg-secondary';
+
+
     const navigate = useNavigate();
+
     function goBack() {
-        navigate('/train')
+        navigate(-1);
     }
 
     return (
-        <div className='w-full flex flex-col justify-center items-center'>
-            <div className='flex w-full items-center justify-evenly pb-4'>
-                <Button onClick={goBack}>Go Back</Button>
-                <h1 className='text-lg font-semibold'>May 3, 2024: Lower Body 1</h1>
-                <Button onClick={goBack}>Finish Workout</Button>
-            </div>
-            <Carousel className="w-[1100px] h-[550px]">
-                <CarouselContent>
-                    {Array.from({ length: 5 }).map((_, index) => (
-                    <CarouselItem key={index} className='basis-full h-full'>
-                        <div className="p-1">
-                        <Card className='h-[550px]' >
-                            <CardContent className="flex h-full items-center justify-center p-6 ">
-                                <div className='flex flex-col'>
-                                    <div className='flex items-center'>
-                                        <h1>1. Back Squat</h1>
-                                        <Button variant='outline' className='ml-2'>History</Button>
-                                    </div>
-                                    
-                                    <div className='flex items-center m-2'>
-                                        <p>Set 1: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
-                                        <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
-                                        <Button variant='outline' className='mx-2'>Add Note</Button>
-                                        <Button variant='outline'>Add Video</Button>
-                                    </div>
-                                    <div className='flex items-center m-2'>
-                                        <p>Set 2: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
-                                        <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
-                                        <Button variant='outline' className='mx-2'>Add Note</Button>
-                                        <Button variant='outline'>Add Video</Button>
-                                    </div>
-                                    <div className='flex items-center m-2'>
-                                        <p>Set 3: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
-                                        <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
-                                        <Button variant='outline' className='mx-2'>Add Note</Button>
-                                        <Button variant='outline'>Add Video</Button>
-                                    </div>
-                                    <div className='flex items-center m-2'>
-                                        <p>Set 4: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
-                                        <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
-                                        <Button variant='outline' className='mx-2'>Add Note</Button>
-                                        <Button variant='outline'>Add Video</Button>
-                                    </div>
-                                    <div className='flex gap-1 items-center' > 
-                                        <Drawer>
-                                            <DrawerTrigger asChild><Button size='lg' variant='outline'><FontAwesomeIcon size='lg' icon={faClock} /></Button></DrawerTrigger>
-                                            <DrawerContent>
-                                                <div className="h-[300px] flex flex-col">
-                                                <DrawerFooter>
-                                                <DrawerClose className='mb-4 self-bottom'> 
-                                                    <Button variant="outline">Cancel</Button>
-                                                </DrawerClose>
-                                                </DrawerFooter>
-                                                </div>
-                                            </DrawerContent>
-                                        </Drawer>
-                                        <Button size='lg' variant='outline'>Save</Button>
-                                    </div>
+        <div className={`w-full ${backgroundColorClass} border rounded-lg p-4`}>
+            <Card className='h-full w-full'>
+                <div className='flex relative w-full items-center justify-center p-4'>
+                    <FontAwesomeIcon className='absolute top-6 left-6' onClick={goBack} size="xl" icon={faAngleLeft} />
+                    <h1 className='text-xl font-semibold'>May 3, 2024: Lower Body 1</h1>
+                </div>
+                <div className='w-full flex flex-col justify-center items-center'>
+                    
+                    <Carousel className="w-[1100px] h-[550px]">
+                        <CarouselContent>
+                            {Array.from({ length: 5 }).map((_, index) => (
+                            <CarouselItem key={index}  >
+                                <div className="p-1">
+                                <Card className='h-[550px]' >
+                                    <CardContent className="flex h-full items-center justify-center p-6 ">
+                                        <div className='flex flex-col'>
+                                            <div className='flex items-center'>
+                                                <h1>1. Back Squat</h1>
+                                                <Button variant='outline' className='ml-2'>History</Button>
+                                            </div>
+                                            
+                                            <div className='flex items-center m-2'>
+                                                <p>Set 1: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
+                                                <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
+                                                <Button variant='outline' className='mx-2'>Add Note</Button>
+                                                <Button variant='outline'>Add Video</Button>
+                                            </div>
+                                            <div className='flex items-center m-2'>
+                                                <p>Set 2: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
+                                                <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
+                                                <Button variant='outline' className='mx-2'>Add Note</Button>
+                                                <Button variant='outline'>Add Video</Button>
+                                            </div>
+                                            <div className='flex items-center m-2'>
+                                                <p>Set 3: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
+                                                <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
+                                                <Button variant='outline' className='mx-2'>Add Note</Button>
+                                                <Button variant='outline'>Add Video</Button>
+                                            </div>
+                                            <div className='flex items-center m-2'>
+                                                <p>Set 4: Reps</p><Label htmlFor="reps" className='mr-2'></Label><Input id='reps' className='w-20 mr-2'></Input>
+                                                <Label htmlFor="weight" className='mr-2'>Weight</Label><Input id='weight' className='w-20'></Input>
+                                                <Button variant='outline' className='mx-2'>Add Note</Button>
+                                                <Button variant='outline'>Add Video</Button>
+                                            </div>
+                                            <div className='flex gap-1 items-center' > 
+                                                <Drawer>
+                                                    <DrawerTrigger asChild><Button size='lg' variant='outline'><FontAwesomeIcon size='lg' icon={faClock} /></Button></DrawerTrigger>
+                                                    <DrawerContent>
+                                                        <div className="h-[300px] flex flex-col">
+                                                        <DrawerFooter>
+                                                        <DrawerClose className='mb-4 self-bottom'> 
+                                                            <Button variant="outline">Cancel</Button>
+                                                        </DrawerClose>
+                                                        </DrawerFooter>
+                                                        </div>
+                                                    </DrawerContent>
+                                                </Drawer>
+                                                <Button size='lg' variant='outline'>Save</Button>
+                                            </div>
+                                        </div>
+                                        
+                                        
+                                    </CardContent>
+                                </Card>
+                                </div>
+                            </CarouselItem>
+                            ))}
+                            <CarouselItem className='basis-full h-full'>
+                                <div className="p-1">
+                                    <Card className='h-[550px]'>
+                                        <CardContent className="flex flex-col h-full items-center justify-center p-6 gap-2">
+                                                <h1 className='text-xl font-semibold'>Workout Finished!</h1>
+                                                <Button onClick={goBack}>End Workout</Button>
+
+                                        </CardContent>
+                                    </Card>
                                 </div>
                                 
-                                
-                            </CardContent>
-                        </Card>
-                        </div>
-                    </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>           
+                            </CarouselItem>
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>           
+                </div>
+            </Card>
         </div>
+        
         
         
     )
