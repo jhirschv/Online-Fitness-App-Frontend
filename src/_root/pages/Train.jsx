f7c0ee19a9db21087321bfc7b73acad4beebd1d0
jhirschv <johnhirschv@gmail.com>
2024-03-06 19:11:31 -0500

    Got the change current workout function to work

--- src/_root/pages/Train.jsx
+++ src/_root/pages/Train.jsx
@@ -52,12 +52,12 @@ import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
 const Train = () => {
     const { theme } = useTheme();
+    const backgroundColorClass = theme === 'dark' ? 'bg-popover' : 'bg-secondary';
     const navigate = useNavigate();
 
     function startTrainingSession() {
         navigate('/workoutSession')
     }
-
     function updateActiveProgram(selectedProgram) {
             const payload = {
                 program_id: selectedProgram, 
@@ -80,7 +80,6 @@ const Train = () => {
         });
         
     }
-
     const startWorkoutSession = () => {
         const payload = {
             workout_id: currentWorkout.id, 
@@ -96,19 +95,75 @@ const Train = () => {
             .catch(error => {
                 console.error('Error starting workout session:', error);
             });
-    };
-
-    const [activeProgram, setActiveProgram] = useState(null)
-    const [selectedProgram, setSelectedProgram] = useState(null)
+    }
     const handleProgramClick = (programId) => {
         setSelectedProgram(programId)
-        console.log(selectedProgram);
-      };
+    }
+    const updateWorkoutProgress = async (selectedWorkout) => {
+        try {
+            const response = await apiClient.post('/update_workout_progress/', {
+                phase_id: selectedWorkout.phaseId,
+                week_number: selectedWorkout.week,
+                workout_id: selectedWorkout.workoutId
+            });
+    
+            // Check if the update was successful
+            if (response.status === 200) {
+                // Re-fetch the current workout to update the UI
+                fetchCurrentWorkout();
+            }
+        } catch (error) {
+            console.error('Failed to update workout progress:', error);
+            // Handle error appropriately
+        }
+    };
+    
+    
+    function handleSelectedWorkout(data) {
+        setSelectedWorkout(data)
+        console.log(data)
+    }
+    const fetchCurrentWorkout = () => {
+        apiClient.get('/current_workout/') // Make sure the endpoint matches your Django URL configuration
+        .then(response => {
+            setCurrentWorkout(response.data);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    };
+    const handleSheetOpenChange = (open) => {
+        setIsSheetOpen(open);
+
+        // If the Sheet is being closed (open is false), reset selectedWorkout
+        if (!open) {
+            setSelectedWorkout(null);
+        }
+    };
+
+    const [date, setDate] = React.useState(new Date())
+    const [activeProgram, setActiveProgram] = useState(null)
+    const [selectedProgram, setSelectedProgram] = useState(null)
     const [userPrograms, setUserPrograms] = useState([])
     const [currentWorkout, setCurrentWorkout] = useState(null);
-    
+    const [phasesDetails, setPhasesDetails] = useState([]);
+    const [selectedWorkout, setSelectedWorkout] = useState(null);
+    const [isSheetOpen, setIsSheetOpen] = useState(false);
 
-
+    useEffect(() => {
+        // Directly using apiClient to fetch data
+        const fetchData = async () => {
+        try {
+            const response = await apiClient.get(`phase_details/1/`);
+            setPhasesDetails(response.data);
+        } catch (error) {
+            console.error('Error fetching phases and workouts:', error);
+            // Optionally handle the error, e.g., updating state to show an error message
+        }
+        };
+        fetchData();
+    }, []); // This effect depends on programId   
     useEffect(() => {
         apiClient.get('/get_active_program/') // Make sure the endpoint matches your Django URL configuration
         .then(response => {
@@ -118,18 +173,9 @@ const Train = () => {
             console.error('Error fetching data:', error);
         });
     }, []);
-
     useEffect(() => {
-        apiClient.get('/current_workout/') // Make sure the endpoint matches your Django URL configuration
-        .then(response => {
-            setCurrentWorkout(response.data)
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+        fetchCurrentWorkout();
     }, [activeProgram]);
-
-    
     useEffect(() => {
         apiClient.get('/user_programs/') // Make sure the endpoint matches your Django URL configuration
         .then(response => {
@@ -139,11 +185,7 @@ const Train = () => {
             console.error('Error fetching data:', error);
         });
     }, []);
-
-
-    const [date, setDate] = React.useState(new Date())
-    // Determine the background color class based on the theme
-    const backgroundColorClass = theme === 'dark' ? 'bg-popover' : 'bg-secondary';
+   
 
     return (
         <div className={`${backgroundColorClass} w-full p-4 border rounded-lg`}>
@@ -183,7 +225,7 @@ const Train = () => {
                             
                             <div className='flex items-center justify-between pr-2'>
                                 {currentWorkout?
-                                <h1 className='font-semibold text-lg'>Lower Body 1</h1>
+                                <h1 className='font-semibold text-lg'>{currentWorkout.name}</h1>
                                 :<h1>No Current Workout</h1>
                                 }
                                 <div>
@@ -216,7 +258,36 @@ const Train = () => {
                         
                         <div className='mb-6'>
                             <Button onClick={startWorkoutSession} className='self-center w-1/2 p-6 text-lg'>Start Training!</Button>
-                            <Button variant='outline' className='mx-2 self-center w-1/3 mb-4 p-6 text-lg'>Skip Workout</Button>
+                            <Sheet open={isSheetOpen} onOpenChange={handleSheetOpenChange}>
+                                <SheetTrigger asChild>
+                                    <Button variant='outline' onClick={() => setIsSheetOpen(true)} className='mx-2 self-center w-1/3 mb-4 p-6 text-lg'>Change Workout</Button>
+                                </SheetTrigger>
+                                <SheetContent>
+                                    <div className='w-full flex justify-center'>
+                                        <Button onClick={() => { updateWorkoutProgress(selectedWorkout); setIsSheetOpen(false); }}  disabled={!selectedWorkout} className='self-center w-1/2 p-6 text-lg'>Save</Button>
+                                    </div>
+                                    <ScrollArea className="h-full w-full">
+                                    <div>
+                                        {/* Render phases, weeks, and workouts based on the fetched data */}
+                                        {phasesDetails.map((phase) => (
+                                            <div key={phase.id}>
+                                            <h3 className='font-bold text-center p-2'>{phase.name}</h3>
+                                            {phase.workouts_by_week.map((week, index) => (
+                                                <div key={index}>
+                                                <h4 className='font-semibold text-center p-2'>Week {week.week}</h4>
+                                                {week.workouts.map((workout) => (
+                                                    <p  onClick={() => handleSelectedWorkout({ phaseId: phase.id, week: week.week, workoutId: workout.id, workoutName: workout.name })}
+                                                    className={`ml-4 p-4 border mr-4 ${selectedWorkout?.workoutId === workout.id && selectedWorkout?.week === week.week ? 'bg-secondary' : 'bg-background'}`} key={workout.id}>{workout.name}
+                                                    </p>
+                                                ))}
+                                                </div>
+                                            ))}
+                                            </div>
+                                        ))}
+                                        </div>
+                                    </ScrollArea>
+                                </SheetContent>
+                             </Sheet>
                         </div>
                         
                     </div>
