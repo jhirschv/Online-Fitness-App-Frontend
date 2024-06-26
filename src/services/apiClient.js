cd1786288ae1fea52bac37c9f6c09a85420c5b98
jhirschv <johnhirschv@gmail.com>
2024-06-26 17:07:09 -0500

    new build

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'http://18.219.101.86/',
 });
 //http://localhost:8000/
 //http://192.168.5.80:8000/
